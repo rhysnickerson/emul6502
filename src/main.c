853f005587ca7b20368f55bbda798ea8efdb908b
rhys <2001rhys@gmail.com>
2021-05-10 09:31:45 -0400

    Added ROM allocation to readProgram

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 #include <stdint.h>
 #include "cpu6502.h"
 
-void readProgram(char* file, c_6502* CPU, uint16_t addr){ //TODO: transfer to external file
+void readProgram(char* file, c_6502* CPU){ //TODO: transfer to external file
+    uint16_t addr = CPU->ROM_HEADER;
     FILE* f = fopen(file,"r");
     if (f == NULL){
         printf("Cannot find file");
@@ -27,9 +28,10 @@ void readProgram(char* file, c_6502* CPU, uint16_t addr){ //TODO: transfer to ex
 }
 //TODO: proper file loading
 int main(int argv, char* argc[]){
-    c_6502* CPU = c_init();
-    readProgram("/home/rhys/CLionProjects/emul6502/src/program",CPU,0x0000);
+    c_6502* CPU = c_init((4*1024));
+    readProgram("/home/rhys/CLionProjects/emul6502/src/program",CPU);
     while(execute(CPU)!=1);
+
     c_destroy(CPU);
 
 }
